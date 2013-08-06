4f262054de648635e26a2fe966338c28900e80f6
Theron Boerner <hunterboerner@gmail.com>
2013-08-06 13:45:45 -0500

    uh I forget...

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 describe "Static pages" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App | " }
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end 
-		it "should have the title 'Home'" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+		end
+		it "should not have a custom page title" do
+	      visit '/static_pages/home'
+	      expect(page).not_to have_title('| Home')
 		end
 	end
 	describe "Help page" do 
@@ -17,7 +22,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title(base_title + "Help")
 		end
 	end
 	describe "About page" do 
@@ -27,7 +32,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'About'" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+			expect(page).to have_title(base_title + "About")
 		end
 	end
 end
