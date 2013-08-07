351f577602cf2fcee3861b8a7a0048c828250bc4
Theron Boerner <hunterboerner@gmail.com>
2013-08-07 13:58:19 -0500

    some tests and a pretty page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Learning::Application.routes.draw do
-  get "users/new"
+	get "users/new"
 	root 'static_pages#home'
 	match '/help',    to: 'static_pages#help',    via: 'get'
 	match '/about',   to: 'static_pages#about',   via: 'get'
-  	match '/contact', to: 'static_pages#contact', via: 'get'
-  # get "static_pages/home"
-  # get "static_pages/help"	
-  # get "static_pages/about" 
-  # get "static_pages/contact"
+	match '/contact', to: 'static_pages#contact', via: 'get'
+	match '/signup',  to: 'users#new',            via: 'get'
 end
