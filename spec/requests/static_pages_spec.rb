f3d1f892e4c2eb34cc53340c5de1f91d871198a8
Theron Boerner <hunterboerner@gmail.com>
2013-08-07 13:03:23 -0500

    Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,4 +35,14 @@ describe "Static pages" do
 			expect(page).to have_title(base_title + "About")
 		end
 	end
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title(base_title + "Contact")
+		end
+	end
 end
