85fb46bcd7ebb268247c6ec122fbe65ec431799e
Theron Boerner <hunterboerner@gmail.com>
2013-08-08 11:47:45 -0500

    foo@foo..com should not be valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 
 	validates :name,  presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	#I was going to put an insane regex here from the internetz but it seemed to take more effort than I wanted to put in now
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
