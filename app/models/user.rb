621d7c0dc1eac43a94a4c30fb9a0d363f2c5355a
Theron Boerner <hunterboerner@gmail.com>
2013-08-08 10:46:35 -0500

    test stuff thingies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	validates :name,  presence: true, length: { maximum: 50 }
-	validates :email, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#I was going to put an insane regex here from the internetz but it seemed to take more effort than I wanted to put in now
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
