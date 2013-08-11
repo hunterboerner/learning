15b695582c9247a408334dfb55ac08b0e3e3651e
Theron Boerner <hunterboerner@gmail.com>
2013-08-10 19:41:44 -0500

    ¿can I haz correct paramz?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(params[user_params])
+        @user = User.new(user_params)
         if @user.save
             #Handle a sucessful save.
         else  
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     end
 
     private
-        def user_params
-          params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 
-	validates :name,  presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-	#I was going to put an insane regex here from the internetz but it seemed to take more effort than I wanted to put in now
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-
-	has_secure_password
-	validates :password, length: { minimum: 6 }
+    validates :name, presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, length: { minimum: 6 }
 end
