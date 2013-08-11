14f4973e8c6ec2fb3db3e6c2e4382e7b7965ac4c
Theron Boerner <hunterboerner@gmail.com>
2013-08-11 09:53:44 -0500

    tests and pretty flashes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,9 +24,15 @@ describe "UserPages" do
 		let(:submit) { "Create my account" }
 
 		describe "with invalid information" do
-		  it "should not create a user" do
-		    expect	{ click_button submit }.not_to change(User, :count)
-		  end
+			it "should not create a user" do
+				expect	{ click_button submit }.not_to change(User, :count)
+			end
+			describe "after submission" do 
+				before { click_button submit }
+
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end
 		end
 
 		describe "with valid information" do
@@ -37,7 +43,14 @@ describe "UserPages" do
 				fill_in "Confirmation", with: "foobar"
 			end
 			it "should create a user" do
-			  expect { click_button submit }.to change(User, :count).by(1)
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
