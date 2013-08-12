7d3f1659f45e965d64faf26eeb4dbb07a0152cb8
Theron Boerner <hunterboerner@gmail.com>
2013-08-12 13:39:23 -0500

    smartgit is annoying me hmmm before create sorry about extra commits but lolcommits smartgit doesnt seem to be working with lolcommits Yay lol-ing! hmm smartgit lolcommits... let's see if lolcommits works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+    before_create :create_remember_token
 
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
