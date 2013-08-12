fe2f0cbb72e62a2e088591873d20967e2b8be221
Theron Boerner <hunterboerner@gmail.com>
2013-08-12 11:34:45 -0500

    display wrong password error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < ApplicationController
-    
+
     def new
     end
 
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            # Sign the user in and redirect to the user's show page.
+        else
+            flash[:error] = 'Invalid email/password combination' # Not quite right!
+            render 'new'
+        end
     end
 
     def destroy
