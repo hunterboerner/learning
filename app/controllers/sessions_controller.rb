f9cba53c93c2343c636b9fb997079ac80e8bf5f0
Theron Boerner <hunterboerner@gmail.com>
2013-08-12 14:28:54 -0500

    flash now instead of flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,9 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-            # Sign the user in and redirect to the user's show page.
+          # Sign the user in and redirect to the user's show page.
         else
-            flash[:error] = 'Invalid email/password combination' # Not quite right!
+            flash.now[:error] = 'Invalid email/password combination' # Not quite right!
             render 'new'
         end
     end
