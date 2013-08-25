0386ff0363630ffa2851bc00f6020c3d2d852a74
Theron Boerner <hunterboerner@gmail.com>
2013-08-25 15:17:58 -0500

    success! flash….

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #Handle a sucessful update
+      flash[:success] = "Profile Updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
