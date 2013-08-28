68670d085ee28111ed1b72c1d7b07798fdce5807
Theron Boerner <hunterboerner@gmail.com>
2013-08-27 22:29:29 -0500

    Prevent non-logged in users from messing with stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -34,7 +36,15 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
+
+  # Before filters
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
 end
