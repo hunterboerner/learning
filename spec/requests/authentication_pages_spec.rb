84100f9165131f336adae6370b56f95c9b0c00d8
Theron Boerner <hunterboerner@gmail.com>
2013-09-07 12:39:14 -0500

    friendly forwarding

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,7 +43,7 @@ describe "Authentication" do
       end
     end
   end
-  
+
   describe "authorization" do
 
     describe "for non-signed users" do
@@ -76,6 +76,26 @@ describe "Authentication" do
       describe "submitting a PATCH request to the Users#update action" do
         before { patch user_path(wrong_user) }
         specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end    
+
+        describe "after signing in" do
+
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+        end    
       end
     end
   end
