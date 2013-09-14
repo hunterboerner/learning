64149aca5d125d870e0180cd47cf0f609b58eb60
Theron Boerner <hunterboerner@gmail.com>
2013-09-14 12:02:05 -0500

    test for right order

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,21 @@ describe User do
 	it { should be_valid }
 	it { should_not be_admin }
 
+	describe "micropost associations" do
+		
+		before { @user.save }
+		let!(:older_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+		end
+		let!(:newer_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+		end
+
+		it "should have the right microposts in the right order" do
+			expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+		end
+	end
+
 	describe "with admin attribute set to 'true'" do
 		before do
 			@user.save!
