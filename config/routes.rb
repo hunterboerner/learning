3f6a49b6d75b9389dc8b08d1351958bd14436dae
Theron Boerner <hunterboerner@gmail.com>
2013-09-15 10:01:43 -0500

    routing the micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Learning::Application.routes.draw do
-	resources :users
-    resources :sessions, only: [:new, :create, :destroy]
-	root 'static_pages#home'
-    match '/signup',   to: 'users#new',            via: 'get'   
-    match '/signin',   to: 'sessions#new',         via: 'get'
-    match '/signout',  to: 'sessions#destroy',     via: 'delete'
-	match '/help',     to: 'static_pages#help',    via: 'get'
-	match '/about',    to: 'static_pages#about',   via: 'get'
-	match '/contact',  to: 'static_pages#contact', via: 'get'
+  resources :users
+  resources :sessions,    only: [:new, :create, :destroy]
+  resources :microposts,  only: [:create, :destroy]
+  root 'static_pages#home'
+  match '/signup',   to: 'users#new',            via: 'get'   
+  match '/signin',   to: 'sessions#new',         via: 'get'
+  match '/signout',  to: 'sessions#destroy',     via: 'delete'
+  match '/help',     to: 'static_pages#help',    via: 'get'
+  match '/about',    to: 'static_pages#about',   via: 'get'
+  match '/contact',  to: 'static_pages#contact', via: 'get'
 end
