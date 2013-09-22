c45691eda1fa7a48d64efd669fb23a36fca42e68
Theron Boerner <hunterboerner@gmail.com>
2013-09-22 09:47:41 -0500

    keep below 80 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,9 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
 
@@ -23,7 +25,7 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
-  end 
+  end
 
   def feed
     # This is preliminary. See "Following users" for the full implementation
@@ -31,7 +33,7 @@ class User < ActiveRecord::Base
   end
 
   def following?(other_user)
-    relationships.find_by(followed_id: other_user.id)    
+    relationships.find_by(followed_id: other_user.id)
   end
 
   def follow!(other_user)
