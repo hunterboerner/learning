31201e960db108c9437d8aa3c7e3815fb3df2f2b
Theron Boerner <hunterboerner@gmail.com>
2013-10-03 11:40:54 -0500

    check for asking to sign in when visiting the page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -66,6 +66,16 @@ describe "Authentication" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe "in the Microposts controller" do
