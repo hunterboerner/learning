c355f222d5aede45c0527ce1eb7e1c7ab46b315b
Theron Boerner <hunterboerner@gmail.com>
2013-10-12 09:30:39 -0500

    tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -160,7 +160,12 @@ describe User do
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
-      
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 
